b662a06935324401b8a8bad0809d6fef29277116
agustint96 <92907673+agustint96@users.noreply.github.com>
2024-02-17 19:40:51 -0300

    a

---
+++ scroleamos.js
@@ -0,0 +1,23 @@
+window.sr=ScrollReveal();
+
+    sr.reveal('.animacion-scroll', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-10px'
+    });
+
+    sr.reveal('.animacion-scroll2', {
+        duration: 1500,
+        origin: 'right',
+        distance: '-100px'
+    });
+    
+    sr.reveal('.animacion-scroll3',{
+        duration:1500,
+        rotate: {
+        
+        x: 1,
+        y: 180,
+        
+        }
+    })
